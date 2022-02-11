58ffceea161149888f43893666e7e22c23c5003b
insideoutzombie <daviddbruno9@gmail.com>
2022-02-11 16:37:00 -0500

    more styling for card and user

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import './UserItem.css';
+import Avatar from "../../shared/components/UIElements/Avatar";
+import "./UserItem.css";
 
-const UsersItem = props => {
-    return (
-        <li className="users-list">
-            <div className="user-item__content">
-                <div className="user-item__image">
-                    <img src={props.image} alt={props.name} />
-                </div>
-                <div className="user-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>
-                        {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
-                    </h3>
-                </div>
-            </div>
-        </li>
-    )
+const UsersItem = (props) => {
+  return (
+    <li className="users-list">
+      <div className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </div>
+    </li>
+  );
 };
 
 export default UsersItem;
