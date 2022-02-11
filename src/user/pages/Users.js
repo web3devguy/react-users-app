cb0adb02547ecd9c36f705d046fda23d04b7cccf
insideoutzombie <daviddbruno9@gmail.com>
2022-02-10 22:37:09 -0500

    adds users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from '../components/UsersList';
+
+
 const Users = () => {
-    return <h2>Users Works!</h2>
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'Rick Sanchez', 
+            image: 'https://www.looper.com/img/gallery/what-the-mistakes-in-rick-morty-could-really-mean/intro-1617842307.jpg', 
+            places: 3 
+        }
+    ];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
