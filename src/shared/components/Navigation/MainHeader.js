a9b81e9a12223bfb68a0b55158c8e69b331a8ce3
insideoutzombie <daviddbruno9@gmail.com>
2022-02-11 16:57:44 -0500

    nun

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import './MainHeader.css';
+
+const MainHeader = props => {};
+    return <
--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 
 const UsersItem = (props) => {
   return (
     <li className="users-list">
-      <div className="user-item__content">
+      <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
@@ -19,7 +20,7 @@ const UsersItem = (props) => {
             </h3>
           </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
